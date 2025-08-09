ad6e964238f82120f87029503d50d097cae575c5
TriggeredGMD <triggeredgmdyt@gmail.com>
2025-08-08 23:10:17 -0700

    scorechange

--- js/score.js
+++ js/score.js
@@ -24,8 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    let score = ((100+250)*Math.sqrt(((200-1)/50)*(1/((Math.exp(((100+250)/(100+1)), 2))-1)))/(Math.sqrt(((x-1)/50)+((200-1)/50)*(1/((Math.exp(((100+250)/(100+1))))-100)))));
 
     score = Math.max(0, score);
 
