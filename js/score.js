2df9654646a28a365dede291c15cbe8a629332cf
TriggeredGMD <triggeredgmdyt@gmail.com>
2025-08-09 22:49:20 -0700

    revert

--- js/score.js
+++ js/score.js
@@ -24,8 +24,8 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = ((100+500)*Math.sqrt(((150-1)/50)*(1/((Math.exp(((100+500)/(100+1), 2), 2))-100))) /
-        (Math.sqrt(((rank-1)/50)+((200-1)/50)*(1/((Math.exp(((100+500)/(100+1)), 2))-100)))));
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
